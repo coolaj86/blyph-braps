fcd1a1b7173375ac93a83d75df5ea5207658ae98
AJ ONeal <coolaj86@gmail.com>
2011-07-20 02:03:49 +0000

    added emailability, byu parsing, isbn recog

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
   "use strict";
 
   var connect = require('connect')
+    , mailer = require('emailjs')
+    , mailserver = mailer.server.connect({
+          user: "aj@blyph.com"
+        , password: "Wh1t3Ch3dd3r"
+        , host: "smtp.gmail.com"
+        , ssl: true
+      })
     , cradle = require('cradle')
     , db = new(cradle.Connection)('coolaj86.couchone.com', 443, {
           secure: true
@@ -11,6 +18,41 @@
     , vhost
     ;
 
+  function handleSchool(school) {
+    // "http://www.byu.edu".match(/.*\b(\w+\.\w+)/);
+    // "byu.edu".match(/.*\b(\w+\.\w+)/);
+    school = school || '';
+    school = school.trim().toLowerCase();
+    school = school.match(/.*\b(\w+\.\w+)/);
+    if (school) {
+      school = school[1];
+    }
+    return school;
+  }
+
+  function handleEmail(email) {
+    email = email.trim().toLowerCase();
+    /*
+    // TODO remove extraneous '.' and trailing '+xyz' for 
+    if (email.match(/@gmail\./) || email.match(/@googlemail\./)) {
+    }
+    */
+    return email;
+  }
+
+  function sendEmail(user, fn) {
+    var headers = {
+            from: "AJ ONeal <aj@blyph.com>"
+          , to: user.email
+          , subject: "Thanks for signing up with Blyph"
+          , text: "email: " + user.email + " \nschool: " + user.school + " \n"
+        }
+        // message.attach_alternative("<html>i <i>hope</i> this works!</html>");
+      , message = mailer.message.create(headers)
+      ;
+
+    mailserver.send(message, fn);
+  }
 
   function log(err, data) {
     console.log('err: ' + err);
@@ -18,6 +60,29 @@
   }
 
   function rest(app) {
+    // TODO allow unsubscribe via email
+    app.put('/unsubscribe', function (req, res) {
+      var email = req.body && req.body.email;
+
+      if (!email) {
+        res.end(JSON.stringify({ error: { message: "no email given" } }));
+        return;
+      }
+
+      db.get(email, function (err, data) {
+        if (!data || !data.doNotMail) {
+          res.end(JSON.stringify({ error: { message: "No subscription found. Please email us if you wish to permanently delete your account" } }));
+          return;
+        }
+
+        data.doNotMail = true;
+        db.save(data.email, data, function (err, data) {
+          res.end(JSON.stringify({email: data.email, couchdb: data}));
+        });
+      });
+      
+    });
+
     app.get('/schools', function (req, res) {
       db.view('schools/all', function (err, schools) {
         res.end(JSON.stringify(schools));
@@ -25,13 +90,25 @@
     });
 
     function handleSignUp(req, res) {
+      var email = req.body && req.body.email;
+      var user = req.body;
+
       // TODO make more robust
       res.writeHead(200, {'Content-Type': 'application/json'});
-      db.save(req.body.email, { email: req.body.email, school: req.body.school }, function (err, data) {
-        console.log('saved');
-        //db.getDoc(req.body.email, function (err, data) {
-          res.end(JSON.stringify({email: req.body.email, couchdb: data}));
-        //});
+
+      // 
+      db.get(user.email, function (err, data) {
+        if (data) {
+          res.end(JSON.stringify({email: user.email, couchdb: data}));
+          return;
+        }
+
+        db.save(user.email, { email: user.email, school: user.school }, function (err, data) {
+          sendEmail(user, function(err, message) {
+            console.log(err || message); 
+          });
+          res.end(JSON.stringify({email: email, couchdb: data}));
+        });
       });
     }
 
@@ -44,22 +121,58 @@
     app.post('/subscribe', handleSignUp);
   };
 
+  function cors(req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      //doop(next);
+      if (req.method.match(/options/i)) {
+        res.end();
+        return;
+      }
+      if (next) {
+          next();
+      }
+  }
+
+  function booklistscraper(req, res, next) {
+    console.log(req);
+    next();
+    /*
+    if (!req.url.match(/^\/booklistscraper/)) {
+      next();
+      return;
+    }
+
+    cors(req, res, function () {
+      if (!req.method.match(/POST/i)) {
+        return next();
+      }
+      console.log('cross-origin request', req.body);
+      res.end('oh happy dagger');
+    });
+    */
+  }
+
   server = connect.createServer(
+    // decode html forms
+      connect.bodyParser()
+
+    // cors uploads
+    , booklistscraper
+
     // images, css, etc
-      connect.static(__dirname + '/public')
+    , connect.static(__dirname + '/public')
 
     , connect.favicon(__dirname + '/public/favicon.ico')
-
-    // decode html forms
-    , connect.bodyParser()
 
     // REST API
     , connect.router(rest)
   );
 
   vhost = connect.createServer(
-    connect.vhost('blyph.com', server),
-    connect.vhost('www.blyph.com', server)
+    connect.vhost('alpha.blyph.com', server)
   );
 
   module.exports = vhost;
--- tests/email-with-google-apps.js
+++ tests/email-with-google-apps.js
@@ -1,38 +1,19 @@
 (function () {
-    var email   = require("emailjs")
-      , server  = email.server.connect({
-        //  user:     "coolaj86@gmail.com"
-        //, password: "bmsrbxzqbcninxwa"
+    var mailer   = require("emailjs")
+      , mailserver  = mailer.server.connect({
             user:     "aj@blyph.com"
           , password: "Wh1t3Ch3dd3r"
-        //, password: "Bad-Pass"
           , host:     "smtp.gmail.com"
           , ssl:      true
         })
       , headers = {
             from:     "AJ ONeal <aj@blyph.com>" 
-          , to:       "Brian Turley <turleybw@gmail.com>"
-        //, cc:       "Djamshid Mavandi <mvndaai@gmail.com>, Alvin ONeal <alvin.oneal@gmail.com>"
-          , subject:  "Test 2"
+          , to:       "AJ ONeal <coolaj86@gmail.com>"
+          , subject:  "Test 4"
           , text:     "Testing from the blyph.com address."
         }
-      , message = email.message.create(headers)
+      , message = mailer.message.create(headers)
       ;
 
-    // create the message
-
-    // attach an alternative html email for those with advanced email clients
-    // message.attach_alternative("<html>i <i>hope</i> this works!</html>");
-
-    // attach attachments because you can!
-    //message.attach("path/to/file.zip", "application/zip", "renamed.zip");
-
-    // send the message and get a callback with an error or details of the message that was sent
-    server.send(message, function(err, message) { console.log(err || message); });
-
-    // you can continue to send more messages with successive calls to 'server.send', 
-    // they will be queued on the same smtp connection
-
-    // or you can create a new server connection with 'email.server.connect' 
-    // to asynchronously send individual emails instead of a queue 
+    mailserver.send(message, function(err, message) { console.log(err || message); });
 }());
