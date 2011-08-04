c29020ea94cb9a087fbf26ec55df9bb9b6e9c724
AJ ONeal <coolaj86@gmail.com>
2011-08-04 05:57:05 +0000

    added bookinfo json tables

--- couch-schema.js
+++ couch-schema.js
@@ -21,6 +21,10 @@
       views: {
         all: {
           map: function (doc) {
+            if (doc.type && 'school' !== doc.type) {
+              return;
+            }
+
             if (!doc.school || doc.email) {
               return;
             }
@@ -32,11 +36,87 @@
     }
   );
 
+  // bookinfo:schema
+  db.save("_design/bookinfo",
+    {
+      views: {
+        all: {
+          map: function (doc) {
+            var info
+              , img
+              ;
+
+            if ('book' !== doc.type) {
+              return;
+            }
+
+            info = doc.info;
+            info.isbn = info.isbn13 || info.isbn10;
+
+            if (!info.isbn) {
+              return;
+            }
+
+            if (info.image) {
+              if (info.image.match(/no_image\.gif$/)) {
+                info.image = '';
+              } else if (img = info.image.match(/ecx.images-amazon.com\/images\/I\/(.*?)\./)) {
+                info.image = 'amz:' + img[1];
+              }
+            }
+
+            // days since the blyph epoch is calculated as follows
+            // 15190 - parseInt(new Date(info.timestamp).valueOf() / (1000 * 60 * 60 * 24))
+            // this is to conserve bytes
+            emit(doc.school, [
+                info.isbn
+              , 15190 - parseInt(new Date(info.timestamp).valueOf() / (1000 * 60 * 60 * 24))
+              , info.title
+              , info.author
+              , info.binding
+              , info.msrp
+            //, info.pages
+              , info.publisher
+              , info.published_date
+              , info.edition
+            //, info.rank
+            //, info.rating
+              , info.image
+            ]);
+          }
+        }
+      }
+    }
+  );
+
+  db.save("_design/booklist",
+    {
+      views: {
+        all: {
+          map: function (doc) {
+            var id = doc.email || doc.student || doc.token;
+
+            if ('booklist' !== doc.type) {
+              return;
+            }
+
+            delete doc.type;
+            emic([id, doc.term], doc);
+          }
+        }
+      }
+    }
+  );
+
   db.save("_design/users",
     {
       views: {
         all: {
           map: function (doc) {
+            if (doc.type && 'user' !== doc.type) {
+              return;
+            }
+
             if (!doc.email) {
               return;
             }
@@ -46,6 +126,10 @@
         },
         bySchool: {
           map: function (doc) {
+            if (doc.type && 'user' !== doc.type) {
+              return;
+            }
+
             if (!doc.email) {
               return;
             }
